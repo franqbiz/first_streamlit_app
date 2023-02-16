760ada8e2fd8eec133989b43d5fc9a321ea11b7a
franqbiz <111460540+franqbiz@users.noreply.github.com>
2023-02-16 16:32:40 -0600

    Update streamlit_app.py
    Add json viz

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,9 @@ streamlit.dataframe(fruits_to_show)
 
 
 # Lesson 9
+streamlit.header("Fruityvice Fruit Advice!")
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
